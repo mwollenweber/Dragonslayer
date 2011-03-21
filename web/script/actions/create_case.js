2ba60962237208b153f8f90467b5d781a7780f69
Brandon Dixon <brandon.s.dixon@gmail.com>
2011-03-21 17:58:06 -0400

    pull

--- web/script/actions/create_case.js
+++ web/script/actions/create_case.js
@@ -292,7 +292,7 @@ SampleApp.CreateCase.FormPanel = function(){
     
 	//values pulled from the global form
 	snatch_user();
-	get_ip_info(victim);
+//	get_ip_info(victim);
 	
 //    new Ext.KeyMap(Ext.get(document), {
 //    	key:'S',
