4a7c8f1c4203fd63639349c4a3aa3be4bb05fb74
Brandon Dixon <brandon.s.dixon@gmail.com>
2011-03-29 11:17:01 -0400

    adjusted count code

--- web/controls/reports/generate_weekly_report_display.php
+++ web/controls/reports/generate_weekly_report_display.php
@@ -64,9 +64,9 @@ if($start_date != null && $end_date != null) {
 	$vip_case_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN '$start_date' AND '$end_date' AND report_category > 200 AND (report_category != 205) AND (report_category != 25)";
 	$mail_compromise_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN '$start_date' AND '$end_date' AND report_category = 200 OR report_category = 25";
 } else {
-	$total_cases_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE()  AND report_category > 1";
-	$student_case_query = "SELECT COUNT(*) as count from gwcases WHERE DATE(tdstamp) BETWEEN SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE() AND report_category = 20";
-	$normal_case_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN  SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE() AND report_category >= 100";
+	$total_cases_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE()  AND report_category > 1 AND (report_category != 205) AND (report_category != 25)";
+	$student_case_query = "SELECT COUNT(*) as count from gwcases WHERE DATE(tdstamp) BETWEEN SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE() AND report_category = 20 AND (report_category != 205) AND (report_category != 25)";
+	$normal_case_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN  SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE() AND report_category >= 100 AND (report_category != 205) AND (report_category != 25)";
 	$vip_case_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) BETWEEN  SUBDATE(CURDATE(), DAYOFWEEK(CURDATE())) and CURDATE() AND report_category > 200";
 	$mail_compromise_query = "SELECT COUNT(*) as count FROM gwcases WHERE DATE(tdstamp) AND report_category = 200 OR report_category = 25";
 }
--- web/scratch_pad.js
+++ web/scratch_pad.js
@@ -1,77 +1,154 @@
-SearchByIpPortlet = function() {
+Named functions single count, total count, average
+==================================================
 
-    searchByIpFormPanel = new SampleApp.SearchByIpPortlet.FormPanel(); //Create the form panel for the search box
-	
-	SampleApp.SearchByIpPortlet.FormPanel = function(){
-
-		SampleApp.SearchByIpPortlet.categories = [
-	       ['dsid', 'DSID'],
-	       ['analyst', 'Analyst'],
-	       ['netid', 'NetID'],
-	       ['dragon_event', 'Event'],
-	       ['victim_ip', 'Victim IP'],
-	       ['attacker_ip', 'Attacker IP'],
-	       ['network', 'Network'],
-	       ['text_in_verification', 'Text in Verification'],
-	   ];
-		
-		SampleApp.SearchByIpPortlet.FormPanel.superclass.constructor.call(this,{
-			frame:true,
-	        buttonAlign : 'left',
-	        bodyStyle:'padding:5px 5px 0',
-	        defaultType: 'textfield',
-	        items: [
-	            new Ext.form.ComboBox({
-	                fieldLabel: 'Type',
-	                name: 'type',
-	                hiddenName: 'search_type',
-	                width: 300,
-	                store: new Ext.data.ArrayStore({
-	                    fields: ['code', 'search_type'],
-	                    data : SampleApp.SearchByIp.categories
-	                }),
-	                valueField:'code',
-	                displayField:'search_type',
-	                typeAhead: true,
-	                allowBlank:false,
-	                mode: 'local',
-	                triggerAction: 'all',
-	                emptyText:'Select a type...',
-	            }),
-	            {
-	                fieldLabel: 'Value',
-	                name: 'search_value',
-	                allowBlank:false,
-	                width: 300
-	            },
-	        ],
-
-	        buttons: [{
-	            text: 'Search',   
-	            formBind: true,	 
-	            handler:function(){ 
-	            	var search_type = searchByIpFormPanel.getForm().findField('search_type');
-	            	var search_value = searchByIpFormPanel.getForm().findField('search_value');
-	            	
-	            	alert(search_type);
-	            },
-	        }],
-	        keys: [
-	               { key: [Ext.EventObject.ENTER], handler: function() {
-		            	var search_type = searchByIpFormPanel.getForm().findField('search_type');
-		            	var search_value = searchByIpFormPanel.getForm().findField('search_value');
-	                   }
-	               }
-	           ],
-	        region: "north",
-	        height: 100
-	    });
-	}
-	
-	SearchByIpPortlet.superclass.constructor.call(this, {
-        height: 95,
-        items: [searchByIpFormPanel]
-    });
+var map = function () {
+    this.structure.keywords.keyword.forEach(
+    	function (z) {
+    		emit(z.name, {count_sum: z.count, count: 1});
+		}
+	);
 }
 
-Ext.extend(SearchByIpPortlet, Ext.Panel);
+var reduce = function (key, values) {
+    var total = 0;
+    var count = 0;
+    var stotal = 0;
+    for (var i = 0; i < values.length; i++) {
+        stotal += values[i].count;
+        total += values[i].count_sum;
+	    count++;
+    }
+    return { count_sum:total, count: stotal };
+}
+
+db.malware.mapReduce(map,reduce, {out: "named_funcs" });
+
+Named functions single count, total count, average (hex encoded)
+================================================================
+
+var map = function () {
+    this.structure.keywords.keyword.forEach(function (z) {emit(z.name, {count_hex: z.hexcodecount, count:1});});
+}
+
+var reduce = function (key, values) {
+    var total = 0;
+    var count = 0;
+    for (var i = 0; i < values.length; i++) {
+if (values[i].count_hex > 0) {
+        total += values[i].count_hex;
+	    count++;
+}
+    }
+	avg = total/count;
+    return { count_hex:total, count: count};
+}
+
+db.malware.mapReduce(map,reduce, {out: "named_func_hex" });
+
+Average filesize
+================
+
+var map = function () {
+    emit({}, {total_file_size: this.structure.filesize, count: 1, avg: 0}); 
+}
+
+var reduce = function (key, values) { 
+   var total = 0; 
+   var count = 0; 
+   for (var i = 0; i < values.length; i++) { 
+       total += parseInt(values[i].total_file_size); 
+       count += values[i].count; 
+   } 
+   avg = total/count; 
+   return { total_file_size: total, avg: avg, count: count }; 
+} 
+
+db.malware.mapReduce(map,reduce, {out: "filesize" });
+
+Average Entropy
+================
+
+var map = function () {
+    emit({}, {tent: this.structure.totalEntropy, sent: this.structure.streamEntropy, nent: this.structure.nonStreamEntropy, total_ent:0,stream_ent:0,nonstream_ent:0,count:1});
+}
+
+var reduce = function (key, values) {
+    var tent = 0;
+    var sent = 0;
+    var nent = 0;
+    var count = 0;
+    for (var i = 0; i < values.length; i++) {
+        tent += parseFloat(values[i].tent);
+        sent += parseFloat(values[i].sent);
+        nent += parseFloat(values[i].nent);
+        count += values[i].count;
+    }
+	tavg = tent/count;
+	savg = sent/count;
+	navg = nent/count;
+    return { total_ent: tavg, stream_ent: savg, nonstream_ent: navg, count: count, tent: tent, sent: sent, nent: nent };
+}
+
+db.malware.mapReduce(map,reduce, {out: "all_entropy" });
+
+Component totals and averages
+================================================================
+
+var map = function () {
+    this.structure.components.component.forEach(
+		function (z) {
+			emit(z.name, {count: 0, total: z.count, avg:0});
+		}
+	);
+}
+
+var reduce = function (key, values) {
+    var count = db.malware.count();
+    var total = 0;
+    for (var i = 0; i < values.length; i++) {
+    		total += values[i].total;
+    }
+	avg = total/count;
+    return { count: count, avg: avg, total: total};
+}
+
+db.malware.mapReduce(map,reduce, {out: "component_totals" });
+
+
+Collective scanning
+====================
+var map = function () { 
+    this.scans.virustotal.report.results.scanners.forEach( 
+        function (z) { 
+        	emit(z.antivirus, {signatures: [z.signature] , count: 1, total: 0}); 
+        } 
+    ); 
+} 
+
+var reduce = function (key, values) { 
+    var count = 0; 
+    var total = 0; 
+    var sigs = []; 
+    values.forEach( 
+    	function(z){ 	
+            total +=  z.count; 
+            z.signatures.forEach(
+        		function(y) {
+        			if(y != "") { sigs.push(y); }
+        		}
+    		)
+        } 
+    ) 
+
+    var newArray=new Array();
+    label:for(var i=0; i<sigs.length;i++ ) {  
+        for(var j=0; j<newArray.length;j++ ) {
+            if(newArray[j]==sigs[i]) 
+                continue label;
+        }
+        newArray[newArray.length] = sigs[i];
+    }
+    
+    count = newArray.length;
+    return { count: count, total: total, signatures: newArray }; 
+} 
