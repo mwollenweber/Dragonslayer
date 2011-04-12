e50c3bdb3d620dc32728f95d17b0c77de94f6746
Brandon Dixon <brandon@9bplus.com>
2011-04-12 14:47:25 -0400

    removed old web

--- web/extjs/examples/form/file-upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-    sleep(1);
-	move_uploaded_file($_FILES["photo-path"]["tmp_name"], "uploads/" . $_FILES["photo-path"]["name"]);
-    
-    echo '{success:true, file:'.json_encode($_FILES['photo-path']["name"]).'}';
-    
-?>
