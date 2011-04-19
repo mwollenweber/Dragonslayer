9d1e3d1c37f54b418db54dd316e80fb4d34932ca
bsdixoner <brandon.s.dixon@gmail.com>
2011-04-19 07:47:19 -0700

    Edited web/controls/actions/edit_case.php via GitHub

--- web/controls/actions/edit_case.php
+++ web/controls/actions/edit_case.php
@@ -13,13 +13,13 @@ header("Content-type: text/json");
 
 $ip_address = addslashes($_POST['ip_address']);
 
-$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE INET_NTOA(victim)='$ip_address' ORDER BY tdstamp";
+$query = "SELECT id, discovered, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE INET_NTOA(victim)='$ip_address' ORDER BY tdstamp";
 
 $data_result = array();
 $result= mysqli_query($link,$query);
 
 while($row = mysqli_fetch_assoc($result)) {
-	$data_result[] = array($row['id'], $row['tdstamp'], $row['reporter'], $row['event'], $row['INET_NTOA(victim)'], $row['INET_NTOA(attacker)'], $row['dns_name'], $row['network'], $row['verification']);
+	$data_result[] = array($row['id'], $row['discovered'], $row['reporter'], $row['event'], $row['INET_NTOA(victim)'], $row['INET_NTOA(attacker)'], $row['dns_name'], $row['network'], $row['verification']);
 }
 
 mysqli_close($link);
