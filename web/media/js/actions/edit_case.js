f0eefd7035ed4397522bbd9bcd3da1036f8ab6bd
Brandon Dixon <brandon@9bplus.com>
2011-06-07 15:49:41 -0400

    fuck it, works with value

--- web/media/js/actions/edit_case.js
+++ web/media/js/actions/edit_case.js
@@ -248,7 +248,7 @@ SampleApp.EditCase.FormPanel = function(){
         emptyText:'Select a category...',
         allowBlank:false,
 	id : 'category_field_hidden',
-	hiddenName:'code',
+	hiddenName:'value',
     });
     
     verification_field = new Ext.form.TextArea({
