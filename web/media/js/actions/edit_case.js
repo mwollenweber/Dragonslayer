ffb5ce65311e703c6c2b8f000501ec8e6b7c3fb8
Brandon Dixon <brandon@9bplus.com>
2011-06-07 15:56:30 -0400

    changes

--- web/media/js/actions/edit_case.js
+++ web/media/js/actions/edit_case.js
@@ -248,7 +248,7 @@ SampleApp.EditCase.FormPanel = function(){
         emptyText:'Select a category...',
         allowBlank:false,
 	id : 'category_field_hidden',
-	hiddenName:'value',
+	hiddenName:'report_category',
     });
     
     verification_field = new Ext.form.TextArea({
