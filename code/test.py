f205ccf7b68b1ea80da4f9ba1dc97fc116d4cf5b
Matthew Wollenweber <mjw@cyberwart.com>
2011-06-13 00:31:06 -0400

    this is what was current on old usb

---
+++ code/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import dragonslayer
+
+x = dragonslayer.dragonslayer()
+x.update_mdl()
+#x.update_shadow()
+#x.load_dragon_events()
+#x.update_patchy()
+#x.generate_hourly_bad()
+x.shutdown()
