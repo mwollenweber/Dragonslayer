6fb5683ad44a4036c30d8685687d5adca236d9e7
Brandon Dixon <brandon.s.dixon@gmail.com>
2011-07-07 14:21:01 -0400

    added category to search results

--- web/controls/actions/search_by_type.php
+++ web/controls/actions/search_by_type.php
@@ -38,27 +38,27 @@ function handle_masking($search_value) {
 }
 
 if($type == "dsid") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE id='$search_value' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE id='$search_value' ORDER BY id DESC";
 } elseif ($type == "analyst") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE reporter='$search_value' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE reporter='$search_value' ORDER BY id DESC";
 } elseif ($type == "netid") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE netid='$search_value' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE netid='$search_value' ORDER BY id DESC";
 } elseif ($type == "event") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE event='$search_value' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE event='$search_value' ORDER BY id DESC";
 } elseif ($type == "victim_ip") {
 	$ip_data = handle_masking($search_value);
 	$ip_address = $ip_data['ip'];
 	$mask = $ip_data['mask'];
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE victim BETWEEN INET_ATON('$ip_address') AND INET_ATON('$ip_address') + $mask ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE victim BETWEEN INET_ATON('$ip_address') AND INET_ATON('$ip_address') + $mask ORDER BY id DESC";
 } elseif ($type == "attacker_ip") {
 	$ip_data = handle_masking($search_value);
 	$ip_address = $ip_data['ip'];
 	$mask = $ip_data['mask'];
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE attacker BETWEEN INET_ATON('$ip_address') AND INET_ATON('$ip_address') + $mask ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE attacker BETWEEN INET_ATON('$ip_address') AND INET_ATON('$ip_address') + $mask ORDER BY id DESC";
 } elseif ($type == "network") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE network='$search_value' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE network='$search_value' ORDER BY id DESC";
 } elseif ($type == "text_in_verification") {
-	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification FROM gwcases WHERE verification regexp '[[:<:]]" . $search_value . "[[:>:]]' ORDER BY id DESC";
+	$query = "SELECT id, tdstamp, reporter, event, INET_NTOA(victim), INET_NTOA(attacker), dns_name, network, verification, report_category FROM gwcases WHERE verification regexp '[[:<:]]" . $search_value . "[[:>:]]' ORDER BY id DESC";
 } else {
 	//TODO handle this
 }
@@ -73,7 +73,43 @@ $result= mysqli_query($link,$query);
 $data_result['total'] = $row_cnt;
 
 while($row = mysqli_fetch_assoc($result)) {
-	$holder[] = array('dsid'=>$row['id'], 'date'=>$row['tdstamp'],'analyst'=>$row['reporter'],'event'=>$row['event'],'victim'=>$row['INET_NTOA(victim)'],'attacker'=>$row['INET_NTOA(attacker)'],'dns'=>$row['dns_name'],'network'=>$row['network'],'confirmation'=>$row['verification']);
+	$category = $row['report_category'];
+	if($category == 200) {
+		$category = "Normal";
+	} elseif ($category == 201) {
+		$category = "Normal - Remedied";
+	} elseif ($category == 20) {
+		$category = "Student";
+	} elseif ($category == 300) {
+		$category = "Server";
+	} elseif ($category == 42) {
+		$category = "Needs Research";
+	} elseif ($category == 100) {
+		$category = "Other - Do Not Ticket";
+	} elseif ($category == 252) {
+		$category = "Other - Please Review";
+	} elseif ($category == 250) {
+		$category = "VIP - Please Review";
+	} elseif ($category == 251) {
+		$category = "VIP - Block/Re-image";
+	} elseif ($category == 253) {
+		$category = "Request Review";
+	} elseif ($category == 500) {
+		$category = "Needs Forensics";
+	} elseif ($category == 510) {
+		$category = "Forensics Ongoing";
+	} elseif ($category == 520) {
+		$category = "Forensics Complete";
+	} elseif ($category == 0) {
+		$category = "Delete";
+	} elseif ($category == 25) {
+		$category = "Mail Compromise - Student";
+	} elseif ($category == 205) {
+		$category = "Mail Compromise - Faculty/Staff";
+	} else {
+		
+	}
+	$holder[] = array('dsid'=>$row['id'], 'date'=>$row['tdstamp'],'analyst'=>$row['reporter'],'event'=>$row['event'],'victim'=>$row['INET_NTOA(victim)'],'attacker'=>$row['INET_NTOA(attacker)'],'dns'=>$row['dns_name'],'network'=>$row['network'],'confirmation'=>$row['verification'],'category'=>$category);
 }
 
 if($row_cnt <= 0) {
--- web/script/actions/search_by_type.js
+++ web/script/actions/search_by_type.js
@@ -189,7 +189,7 @@ Ext.extend(SampleApp.SearchByIp.FormPanel, Ext.FormPanel, {
 SampleApp.SearchByIp.GridPanel = function() {
 
 	store = new Ext.data.JsonStore({
-	    fields: ['dsid','date','analyst','event','victim','attacker','dns','network','confirmation'],
+	    fields: ['dsid','date','analyst','event','victim','attacker','dns','network','confirmation','category'],
 	    remoteSort:true,
 	    totalProperty:'total',
 	    root:'results',
@@ -199,13 +199,14 @@ SampleApp.SearchByIp.GridPanel = function() {
 	var cm = new Ext.grid.ColumnModel([ 
 	    { header : 'DSID', width : 100, sortable : true, dataIndex: 'dsid' },
 		{ header : 'Date', width : 160, sortable : true, dataIndex: 'date' },
-		{ header : 'Analyst', width : 200, sortable : true, dataIndex: 'analyst' },
-		{ header : 'Event', width : 120, sortable : true, dataIndex: 'event'},
+		{ header : 'Analyst', width : 100, sortable : true, dataIndex: 'analyst' },
+		{ header : 'Event', width : 150, sortable : true, dataIndex: 'event'},
 		{ header : 'Victim', width : 120, sortable : true, dataIndex: 'victim' },
-		{ header : 'Attacker', width : 170, sortable : true, dataIndex: 'attacker'},
-		{ header : 'DNS', width : 170, sortable : true, dataIndex: 'dns'},
+		{ header : 'Attacker', width : 120, sortable : true, dataIndex: 'attacker'},
+		{ header : 'DNS', width : 100, sortable : true, dataIndex: 'dns'},
 		{ header : 'Network', width : 170, sortable : true, dataIndex: 'network'},
-		{ header : 'Confirmation', width : 170, sortable : true, dataIndex: 'confirmation', id: 'search_by_ip_confirmation',renderer: renderConfirmation}
+		{ header : 'Confirmation', width : 200, sortable : true, dataIndex: 'confirmation', id: 'search_by_ip_confirmation',renderer: renderConfirmation},
+		{ header : 'Category', width : 170, sortable : true, dataIndex: 'category'},
 	]);
 	cm.defaultSortable = true; 
     
