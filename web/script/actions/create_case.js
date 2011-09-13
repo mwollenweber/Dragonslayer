ed6761d786dcc3f23145d945803d5c8630f97aa5
Brandon Dixon <brandon.s.dixon@gmail.com>
2011-09-13 13:48:35 -0400

    final test

--- web/script/actions/create_case.js
+++ web/script/actions/create_case.js
@@ -24,6 +24,9 @@ var category_field;
 var verification_field;
 var dsid_field;
 
+var victim = "";
+var event = "";
+
 
 /**
  * Attach the launcher panel to the West Panel
@@ -61,6 +64,8 @@ SampleApp.CreateCase.OpenFromGrid = function(date,event,victim,attacker,notes) {
 	victim_field.setValue(victim);
 	attacker_field.setValue(attacker);
 	notes_field.setValue(notes);
+	victim_g = victim;
+	event_g = event;
     SampleApp.Main.CenterPanelInstance.add(createCasePanel);
     SampleApp.Main.CenterPanelInstance.activate(createCasePanel);
 }
@@ -418,6 +423,7 @@ Ext.extend(SampleApp.CreateCase.FormPanel, Ext.FormPanel, {
 });
 
 SampleApp.CreateCase.DragonInterface = function(){
+	alert(victim_g);
 	SampleApp.CreateCase.DragonInterface.superclass.constructor.call(this,{
         frame:true,
         html: "<iframe height=100% width=100% src='https://128.164.11.22:9443'></iframe>"
